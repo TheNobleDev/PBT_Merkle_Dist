b26bfdf98a2c9f3eff9b94e10d905583e3fe5aa8
TheNobleDev <piyushg.dev@gmail.com>
2022-02-26 17:06:43 +0530

    Add all code and instructions

---
+++ step5_whales/index.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const BigNumber = require("bignumber.js");
+
+BigNumber.config({ DECIMAL_PLACES: 9 })
+let data = fs.readFileSync("whales.csv").toString().split('\n');
+
+let outFileJSON = "result_combined.json"
+
+let myJSONReal = {
+	decimals : 18,
+	airdrop : {}
+};
+
+for (var i = 0; i < data.length; i++) {
+	let obj = data[i].trim();
+	if(obj.startsWith("\"address\"") || obj == "") {
+		continue;
+	}
+
+	let elements = obj.split(";")
+	if(elements.length != 7 && obj != "") {
+		console.log("Error in file formatting! Line: ", i);
+		process.exit(1);
+	}
+
+	let address = elements[0].slice(1, -1).toLowerCase();
+	let pbtAmtString = elements[elements.length-1].slice(1, -1);
+
+	pbtAmtString = BigNumber(pbtAmtString).toString(10);
+	let pbtAmtNum = BigNumber(pbtAmtString).toNumber();
+    myJSONReal.airdrop[address] = pbtAmtNum;
+}
+
+fs.writeFile(outFileJSON, JSON.stringify(myJSONReal), err => {
+    if (err) return console.log(err);
+    console.log('output JSON file successfully written!\n');
+});
